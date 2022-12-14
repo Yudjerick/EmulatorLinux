04276864b858f269b8aaca9be34e54acfa7d7920
Yudjerick <denispetrenko2004@gmail.com>
2022-12-14 15:14:56 +0300

    improved path parsing

--- emulator.py
+++ emulator.py
@@ -3,12 +3,18 @@ import zipfile
 
 def execute(zipfile):
     namelist = zipfile.namelist()
-    wd = namelist[0].split('/')[0] + '/'
-    root_lvl = wd
+    for i in range(len(namelist)):
+        namelist[i] = '/' + namelist[i]
+    wd = '/' + namelist[0].split('/')[1] + '/'
+    namelist.append(wd)
+    namelist.append('')
+    root_lvl = '/'
     print(namelist)
-
+    print(wd)
     while(True):
-        if('/' not in wd[:-1]):
+        if(wd ==  '/'):
+            invite = '/'
+        elif('/' not in wd[:-1]):
             invite = '~'
         else:
             invite = wd.split('/')[len(wd.split('/'))-2]
@@ -16,9 +22,8 @@ def execute(zipfile):
         words = comand.split()
         if(len(words) == 0):
             break
-            #continue
         elif words[0] == 'pwd':
-            print('/' + wd[:-1])
+            print(wd[:-1])
         elif words[0] == 'ls':
             items = []
             for i in namelist:
@@ -28,29 +33,55 @@ def execute(zipfile):
                         items.append(item)
             print(' '.join(items))
         elif words[0] == 'cat' and len(words) == 2:
-            path = wd + words[1]
-            if(path in namelist):
-                with zipfile.open(path) as myfile:
+            path, pathtype = parse_path(words[1],wd[:-1],namelist)
+            if(pathtype == 'file'):
+                with zipfile.open(path[1:]) as myfile:
                     content = myfile.read()
                     print(content.decode('utf-8'))
-        elif words[0] == 'cd' and len(words) == 2:
-            if words[1] == '.':
-                continue
-            elif words[1] == '..':
-                if wd == root_lvl:
-                    continue
-                wd_splt = wd.split('/')
-                wd_splt.pop()
-                wd_splt.pop()
-                wd = '/'.join(wd_splt) + '/'
-            if words[1][0] == '/':
-                nwd = words[1][1:] + '/'
-                if nwd in namelist:
-                    wd = nwd
+            elif(pathtype == 'directory'):
+                print('cat:' + path + ': it is not file')
             else:
-                nwd = wd + words[1] + '/'
-                if nwd in namelist:
-                    wd = nwd
+                print('cat:' + words[1] + ': no such file or directory')
+        elif words[0] == 'cd' and len(words) == 2:
+            if words[1][0] == '/':
+                None
+            else:
+                path, pathtype = parse_path(words[1],wd[:-1],namelist)
+                print(path)
+                if(pathtype == 'directory'):
+                    wd = path + '/'
+                elif(pathtype == 'file'):
+                    print('cd:' + path + ': it is not directory')
+                else:
+                    print('cd:' + words[1] + ': no such file or directory')
+        else:
+            print('not a command')
+            
+def parse_path(path, wd, namelist):
+    #print('path: '+ path+' wd: ' + wd)   
+    if path == '':
+        if wd + '/' in namelist :
+            return wd,'directory'
+        elif wd in namelist:
+            return wd, 'file'
+        else:
+            return 'x',''
+    command = path.split('/')[0]
+    #print('command= ' + command)
+    if command == '.' :
+        None
+    elif command == '..':
+        wd = wd[:wd.rindex('/')]
+    else:
+        wd += '/' + command
+    if wd + '/' in namelist or wd in namelist:
+        if path.find('/') == -1:
+            path = ''
+        else:
+            path = path[path.find('/')+1:]
+        return parse_path(path, wd, namelist)
+    else:
+        return 'n',''
 
 def run_emulation(archive):
     try:
@@ -59,8 +90,12 @@ def run_emulation(archive):
     except Exception as ex:
         print(ex)
 
-if __name__ == '__main__':
+run_emulation('fs.zip')
+
+
+"""if __name__ == '__main__':
     if len (sys.argv)!=2:
         print("Invalid argument")
     else:
         run_emulation(sys.argv[1])
+"""
