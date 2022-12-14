b98e064c3d282ab6a89f36f2723e906ba1f8cb61
Yudjerick <denispetrenko2004@gmail.com>
2022-12-15 02:03:27 +0300

    fixed all

--- emulator.py
+++ emulator.py
@@ -7,14 +7,14 @@ def execute(zipfile):
         namelist[i] = '/' + namelist[i]
     wd = '/' + namelist[0].split('/')[1] + '/'
     namelist.append(wd)
-    namelist.append('')
+    namelist.append('/')
     root_lvl = '/'
-    print(namelist)
-    print(wd)
+    #print(namelist)
+    #print(wd)
     while(True):
         if(wd ==  '/'):
             invite = '/'
-        elif('/' not in wd[:-1]):
+        elif('/' not in wd[1:-1]):
             invite = '~'
         else:
             invite = wd.split('/')[len(wd.split('/'))-2]
@@ -23,7 +23,10 @@ def execute(zipfile):
         if(len(words) == 0):
             break
         elif words[0] == 'pwd':
-            print(wd[:-1])
+            if(wd == '/'):
+                print(wd)
+            else:
+                print(wd[:-1])
         elif words[0] == 'ls':
             items = []
             for i in namelist:
@@ -44,16 +47,15 @@ def execute(zipfile):
                 print('cat:' + words[1] + ': no such file or directory')
         elif words[0] == 'cd' and len(words) == 2:
             if words[1][0] == '/':
-                None
+                path, pathtype = parse_path(words[1][1:],'',namelist)
             else:
                 path, pathtype = parse_path(words[1],wd[:-1],namelist)
-                print(path)
-                if(pathtype == 'directory'):
-                    wd = path + '/'
-                elif(pathtype == 'file'):
-                    print('cd:' + path + ': it is not directory')
-                else:
-                    print('cd:' + words[1] + ': no such file or directory')
+            if(pathtype == 'directory'):
+                wd = path + '/'
+            elif(pathtype == 'file'):
+                print('cd:' + path + ': it is not directory')
+            else:
+                print('cd:' + words[1] + ': no such file or directory')
         else:
             print('not a command')
             
@@ -71,7 +73,10 @@ def parse_path(path, wd, namelist):
     if command == '.' :
         None
     elif command == '..':
-        wd = wd[:wd.rindex('/')]
+        try:
+            wd = wd[:wd.rindex('/')]
+        except:
+            return wd, 'directory'
     else:
         wd += '/' + command
     if wd + '/' in namelist or wd in namelist:
@@ -90,12 +95,11 @@ def run_emulation(archive):
     except Exception as ex:
         print(ex)
 
-run_emulation('fs.zip')
+#run_emulation('fs.zip')
 
 
-"""if __name__ == '__main__':
+if __name__ == '__main__':
     if len (sys.argv)!=2:
         print("Invalid argument")
     else:
         run_emulation(sys.argv[1])
-"""
